ba9f8f10c7bcbf11fb3a4a53aa94c3522a708471
Ghost <ghuuoster@gmail.com>
2025-03-26 21:09:19 +0100

    Update Mapper-example.lua

--- Mapper-example.lua
+++ Mapper-example.lua
@@ -1,16 +1,17 @@
 local Colors = {
-  ["Red"] = Color3.fromRGB(1,0,0),
-  ["Green"] = Color3.fromRGB(0,1,0),
-  ["Blue"] = Color3.fromRGB(0,0,1)
+	["Red"] = Color3.fromRGB(255,0,0),
+	["Green"] = Color3.fromRGB(0,255,0),
+	["Blue"] = Color3.fromRGB(0,0,255)
 }
 
 local function Mapper(value)
-  if not Colors[value] then
-    print("Error fetching value from table.")
-    return
-  end
+	if not Colors[value] then
+		print("Error fetching value from table.")
+		return
+	end
 
-  return Colors[value]
+	return Colors[value]
 end
 
 print(Mapper("Red")) -- // Will return "1,0,0"
+workspace.Baseplate.Color = Mapper("Red") -- // Makes the baseplate red
